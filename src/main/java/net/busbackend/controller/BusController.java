ff95b2fda00f82c93b238b41734cea8b01e12526
Aryanyadav99 <aryankietstudent@gmail.com>
2025-05-17 16:09:36 +0530

    adding response model

--- src/main/java/net/busbackend/controller/BusController.java
+++ src/main/java/net/busbackend/controller/BusController.java
@@ -1,8 +1,10 @@
 package net.busbackend.controller;
 
 import net.busbackend.entites.Bus;
+import net.busbackend.models.ResponseModel;
 import net.busbackend.services.BusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,9 +18,11 @@ public class BusController {
     //now create two endpoints
 
     @PostMapping("/add")
-    public String addBus(@RequestBody Bus bus){
-        busService.addBus(bus);
-        return "Bus Saved Successfully";
+    public ResponseModel<Bus> addBus(@RequestBody Bus bus){
+        //insted odf returning only string we got a repose model
+        final Bus savedbus=busService.addBus(bus);
+        //this saved bus came from service and then jpa send the bus table which also consist the id so we use it instead if bus
+        return new ResponseModel<>(HttpStatus.OK.value(),"Bus Saved",savedbus);
     }
 
     @GetMapping("/all")
---
+++ src/main/java/net/busbackend/models/ResponseModel.java
@@ -0,0 +1,18 @@
+package net.busbackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseModel<T> {
+    //generic t refer it accpt any type of object either bus route and other
+    private int statusCode;
+    private String message;
+    private T response;
+
+}

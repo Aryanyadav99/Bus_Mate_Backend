cb5b14db86d5f03b1909fe02cfe8fa8ff6887b21
Aryanyadav99 <aryankietstudent@gmail.com>
2025-06-02 15:22:41 +0530

    modifiying

--- src/main/java/net/busbackend/controller/AuthController.java
+++ src/main/java/net/busbackend/controller/AuthController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController
 {
-    @Value("${app-jwt-expiration-milliseconds}")
+    @Value("${app.jwt.expiration-milliseconds}")
     private Long expiration;
 
     @Autowired

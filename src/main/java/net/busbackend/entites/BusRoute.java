8bcb17acd44080106a07873aa238d8b40f60ac1c
Aryanyadav99 <aryankietstudent@gmail.com>
2026-04-15 01:45:30 +0530

    refactor(BusRoute): remove redundancy ,  add constrains and audit

--- src/main/java/net/busbackend/entites/BusRoute.java
+++ src/main/java/net/busbackend/entites/BusRoute.java
@@ -4,23 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="bus_route")
+@Table(name="bus_route",
+    uniqueConstraints = {@UniqueConstraint(columnNames = {"city_from", "city_to"})
+    })
 public class BusRoute {
 
+    // fixes - remove route name because it's a combination of city from and city to which cause redundancy  and provide the
+    // unique constraints on pair city from and city to because we don't want to have duplicate routes between same cities
+    // it also creates indexes so tc became log(n) form n
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long routeId;
-    private String routeName;
+
+    @Column(name = "city_from",nullable = false)
     private String cityFrom;
+
+    @Column(name = "city_to",nullable = false)
     private String cityTo;
+
+    @Column(name = "distance_in_km",nullable = false)
     private Double distanceInKm;
 
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }

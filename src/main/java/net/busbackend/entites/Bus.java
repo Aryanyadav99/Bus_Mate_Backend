9519749057c2d0a629c7f13b04a6e92ed9ea7709
Aryanyadav99 <aryankietstudent@gmail.com>
2026-04-15 01:25:35 +0530

    refactor(bus): improve entity with constrain and audit fields

--- src/main/java/net/busbackend/entites/Bus.java
+++ src/main/java/net/busbackend/entites/Bus.java
@@ -4,28 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "bus")
+@Entity
+@Table(name="bus")
 public class Bus {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long busId;
 
-    @Column(name = "bus_name")
+    @Column(name = "bus_name", nullable = false)
     private String busName;
 
+    @Column(name = "bus_type", nullable = false)
     private String busType;
 
+    @Column(name = "bus_number", nullable = false, unique = true)
+    private String busNumber;
+
+    @Column(name = "total_seat", nullable = false)
     private Integer totalSeat;
 
-    @Column(unique = true)
-    private String busNumber;
+    // for audit purpose
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }

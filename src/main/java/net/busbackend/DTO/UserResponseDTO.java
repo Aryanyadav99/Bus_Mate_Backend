d7ea858a0588bb600923b2906d8ea55a13fc97f0
Aryanyadav99 <aryankietstudent@gmail.com>
2026-04-15 15:45:08 +0530

    feat(DTO): add user req and resp dto

---
+++ src/main/java/net/busbackend/DTO/UserResponseDTO.java
@@ -0,0 +1,19 @@
+
+package net.busbackend.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String id;
+    private String email;
+    @JsonProperty("isAdmin")
+    private boolean isAdmin;
+
+}

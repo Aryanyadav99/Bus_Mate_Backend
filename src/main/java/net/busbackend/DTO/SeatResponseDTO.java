098abd6ee216e435a21a8db0a3e716c1d010f169
Aryanyadav99 <aryankietstudent@gmail.com>
2026-04-18 02:54:25 +0530

    feat(seat):implement dto for seat

---
+++ src/main/java/net/busbackend/DTO/SeatResponseDTO.java
@@ -0,0 +1,15 @@
+package net.busbackend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatResponseDTO {
+    private Integer seatNumber;
+    private String status;
+}
